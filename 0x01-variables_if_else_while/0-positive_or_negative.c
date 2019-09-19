a16f413765a7f40c6fe6f3bc4172bce5f41c1855
Diego Alarcon <1153@holbertonschool.com>
2019-09-19 16:08:36 +0000

    fixing 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,11 +13,12 @@ int main(void)
 	if (n > 0)
 	{
 		printf("is positive");
-	} else if (n == 0) {
+	} else if (n == 0)
+	{
 		printf("is zero");
-	} else {
+	} else
+	{
 		printf("is negative");
 	}
-
 	return (0);
 }
