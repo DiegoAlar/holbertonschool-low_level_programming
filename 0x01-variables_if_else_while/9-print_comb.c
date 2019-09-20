a02d3c386caca866ed52d4c19d920c75d391e384
Diego Alarcon <1153@holbertonschool.com>
2019-09-20 02:28:27 +0000

    adding exercise 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main - void
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num;
+
+	num = 48;
+	do
+	{
+		putchar(num);
+		num++;
+		if (num < 58)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	while (num < 58);
+	putchar('\n');
+
+	return (0);
+}
