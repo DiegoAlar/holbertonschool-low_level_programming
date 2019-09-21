ef250a2b24f3f819fd63e780aab1a27a36dcee53
Diego Alarcon <1153@holbertonschool.com>
2019-09-21 19:06:20 +0000

    fixing 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+/**i
+*main - void
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int first;
+	int second;
+	int count;
+
+	first = 48;
+	second = 48;
+	count = 2;
+	while(first < 58)
+	{
+		while (second < 58)
+		{
+			
+			putchar(first);
+			putchar(second);
+			if((first != 56) && (count != 10))
+			{
+				putchar(',');
+				putchar(' ');			
+			}	
+			second++;
+		}
+		second = 48 + count;
+		first++;
+		count++;
+	}
+	putchar('\n');
+	return (0);
+}
