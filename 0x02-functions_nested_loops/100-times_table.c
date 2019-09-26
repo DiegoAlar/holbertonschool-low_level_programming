46479f4bb7f4aff6900b601a5d9f6c9b94347e21
Diego Alarcon <1153@holbertonschool.com>
2019-09-26 02:56:39 +0000

    fixing 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,9 +24,9 @@ void print_times_table(int n)
 	if (n >= 0 && n <= 14)
 	{
 		for (count1 = 0; count1 <= n; count1++)
-		{	_putchar(number + '0');
-			_putchar(',');
-			_putchar(' ');
+		{
+			_putchar(number + '0');
+			printChars(n, count1);
 			for (count2 = 1; count2 <= n; count2++)
 			{
 				number = number + multiplier;
