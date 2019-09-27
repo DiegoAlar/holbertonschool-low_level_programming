0c12db6cf46ec5969956507853c126b2264c66f3
Diego Alarcon <1153@holbertonschool.com>
2019-09-27 00:32:48 +0000

    fixing 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,9 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
-* function -
-* @param:
-* @param:
-* Return:
+* main - principal function of a program
+*
+* Return: Always 0 - success
 */
 int main(void)
 {
@@ -13,18 +12,18 @@ int main(void)
 	num = factor;
 	mult = 1;
 	div = 2;
-	while(mult != factor)
+	while (mult != factor)
 	{
 		if ((num % div) == 0)
 		{
 			num = num / div;
-                        mult = mult * div;
-                }
-                else
+			mult = mult * div;
+		}
+		else
 		{
 			impar = num;
 			div++;
-			if((mult * div) == factor)
+			if ((mult * div) == factor)
 				break;
 		}
 	}
