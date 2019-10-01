605980d3db2dcc2c705c0d7952fc3f69ba4d8051
Diego Alarcon <1153@holbertonschool.com>
2019-10-01 19:08:09 +0000

    fixing 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-* print_array -
+* print_array - prints numbers of arr given a number
 * @a: the array parameter
 * @n: the number of objects of the array to print
 */
@@ -12,7 +12,7 @@ void print_array(int *a, int n)
 
 	while (i < n)
 	{
-		if(i != (n - 1))
+		if (i != (n - 1))
 		{
 			printf("%i, ", a[i]);
 		}
