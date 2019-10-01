39526d566738e419c97b9bbdfa0546fa044e82ac
Diego Alarcon <1153@holbertonschool.com>
2019-10-01 19:05:46 +0000

    adding exercise 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* print_array -
+* @a: the array parameter
+* @n: the number of objects of the array to print
+*/
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		if(i != (n - 1))
+		{
+			printf("%i, ", a[i]);
+		}
+		else
+		{
+			printf("%i", a[i]);
+		}
+
+		i++;
+	}
+	printf("\n");
+}
