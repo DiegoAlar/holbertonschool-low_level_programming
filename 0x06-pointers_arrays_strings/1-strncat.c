148cd1478e9c93d07441edd0846faa1678f67ede
Diego Alarcon <1153@holbertonschool.com>
2019-10-03 17:05:27 +0000

    fixing 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,25 +1,25 @@
 #include "holberton.h"
 /**
-  * _strcat - appends one string to the other
+  * _strncat - appends one string to the other
   * @dest: the string to be merged
-  * @param: the string to append
+  * @src: the string to append
   * @n: numbers of chars to be appended
   * Return: the string with the merged content
   */
 char *_strncat(char *dest, char *src, int n)
 {
-	        int i = 0, k = 0;
+	int i = 0, k = 0;
 
-		while (dest[i] != '\0')
-		{
-			i++;
-		}
-		while (src[k] != '\0' && k < n)
-		{
-			*(dest + i) = *(src + k);
-			k++;
-			i++;
-		}
-		*(dest + i) = '\0';
-		return dest;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[k] != '\0' && k < n)
+	{
+		*(dest + i) = *(src + k);
+		k++;
+		i++;
+	}
+	*(dest + i) = '\0';
+	return (dest);
 }
