366c4023a41a8fe14317954a8e1cc80e5e10b990
Diego Alarcon <1153@holbertonschool.com>
2019-10-03 17:09:04 +0000

    fixing 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,11 +9,11 @@ char *_strcat(char *dest, char *src)
 {
 	int i = 0, k = 0;
 
-	while (dest[i] != 32)
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (src[k] != 0)
+	while (src[k] != '\n')
 	{
 		i++;
 		dest[i] = src[k];
