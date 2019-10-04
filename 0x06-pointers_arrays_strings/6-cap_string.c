a1cb393e9a8a1e1c8f85dd1f625ec9c20f628244
Diego Alarcon <1153@holbertonschool.com>
2019-10-04 03:14:22 +0000

    adding exercise 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+* cap_string - capitalizes initial letters of a string
+* @s1: string as argument
+* Return: string with changes
+*/
+char *cap_string(char *s1)
+{
+	int k = 0;
+
+	while (s1[k] != '\0')
+	{
+		switch (s1[k])
+		{
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			case 32:
+			case 10:
+			if ((s1[k + 1] > 96 && s1[k + 1] < 123))
+			{
+			s1[k + 1] = s1[k + 1] - 32;
+			}
+			break;
+
+			case 9:
+			s1[k] = 32;
+			break;
+		}
+		k++;
+	}
+	return (s1);
+}
