20925beb0f3e025fbf67feda2908bf14fa34e8bb
Diego Alarcon <1153@holbertonschool.com>
2019-10-05 22:06:16 +0000

    adding exercise 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* leet - encodes strint to 1337
+* @s1: string to be encripted
+* Return: the changed string
+*/
+char *leet(char *s1)
+{
+	char str1[] = "aAeEoOtTiL";
+	char str2[] = "4433007711";
+	int i = 0, k;
+
+	while (s1[i] != '\0')
+	{
+		for (k = 0; str1[k] != '\0'; k++)
+		{
+			if (str1[k] == s1[i])
+			{
+				s1[i] = str2[k];
+			}
+		}
+		i++;
+	}
+return (s1);
+}
