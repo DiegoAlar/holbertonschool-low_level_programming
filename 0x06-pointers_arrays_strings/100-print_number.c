2b8849523ce4e96c415a58342f1bc0811006ea3b
Diego Alarcon <1153@holbertonschool.com>
2019-10-06 03:14:25 +0000

    fixing 100

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -5,30 +5,17 @@
 */
 void print_number(int n)
 {
-	int digit, countDigits = n, count = 0, div = n, i;
+	unsigned int new = n;
 
-	if (n >= 0)
+	if (n < 0)
 	{
-		while (countDigits)
-		{
-			countDigits /= 10;
-			count++;
-		}
-		for (digit = 1; digit < count; digit++)
-		{
-			div = n;
-			for (i = 1; i <= (count - digit); i++)
-			{
-				div /= 10;
-			}
-			_putchar('0' + (div % 10));
-		}
-		_putchar('0' + (n % 10));
-	}
-	else
-	{
-		n *= -1;
 		_putchar('-');
-		print_number(n);
+		new *= -1;
 	}
+
+	if ((new / 10) > 0)
+	{
+		print_number(new / 10);
+	}
+	_putchar((new % 10) + '0');
 }
