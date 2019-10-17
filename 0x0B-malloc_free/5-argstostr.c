16f4f14820bacd9e45188358ed9c74097c93ad3f
Diego Alarcon <1153@holbertonschool.com>
2019-10-17 03:25:55 +0000

    adding exercise 5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+* argstostr - returns a pointer to a 2 dimensional array of integers.
+* @ac: the withdh parameter
+* @av: the height parameter
+* Return: a bidimentional array
+*/
+char *argstostr(int ac, char **av)
+{
+	int i, k = 0, j, c = 0;
+	char *ptr;
+
+	for (i = 0; i < ac; i++)
+	{
+		while (av[i][j])
+		{
+			c++;
+			j++;
+		}
+		j = 0;
+	}
+	j = 0;
+	ptr = malloc(c * sizeof(char) + ac + 1);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		while (av[i][j])
+		{
+			*(ptr + k) = av[i][j];
+			j++;
+			k++;
+		}
+		*(ptr + k) = '\n';
+		j = 0;
+		k++;
+	}
+	*(ptr + k) = '\0';
+	return (ptr);
+}
