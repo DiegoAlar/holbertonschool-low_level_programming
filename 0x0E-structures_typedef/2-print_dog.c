2fb5b58f590d4134efb16deb3b7805ef4c343149
Diego Alarcon <1153@holbertonschool.com>
2019-10-21 21:43:32 +0000

    fixing 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,21 +8,22 @@ void print_dog(struct dog *d)
 {
 	if (d != 0)
 	{
-		if (d->name != 0 && d->age != 0 && d->owner != 0)
+		if (d->name != 0)
 		{
-			printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
+			printf("Name: %s", d->name);
 		}
-		else if (d->name == 0 && d->age != 0 && d->owner != 0)
+		else
 		{
-			printf("Name: (nil)\nAge: %.6f\nOwner: %s\n", d->age, d->owner);
+			printf("Name: (nil)");
 		}
-		/*else if (d->name != 0 && d->age == 0 && d->owner != 0)
+		printf("\nAge: %.6f\n", d->age);
+		if (d->owner != 0)
 		{
-			printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
-		}*/
-		else if (d->name != 0 && d->age != 0 && d->owner == 0)
+			printf("Owner: %s\n", d->owner);
+		}
+		else
 		{
-			printf("Name: %s\nAge: %.6f\nOwner: (nil)\n", d->name, d->age);
+			printf("Owner: (nil)\n");
 		}
 	}
 }
