93fe2d35b1f71266c9dab2e111c1cb3e9d06f2c9
Diego Alarcon <1153@holbertonschool.com>
2019-10-21 21:31:06 +0000

    adding exercise 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,20 +6,23 @@
   */
 void print_dog(struct dog *d)
 {
-	if (d->name != 0 && d->age != 0 && d->owner != 0)
+	if (d != 0)
 	{
-		printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
-	}
-	else if (d->name == 0 && d->age != 0 && d->owner != 0)
-	{
-		printf("Name: (nil)\nAge: %.6f\nOwner: %s\n", d->age, d->owner);
-	}
-	else if (d->name != 0 && d->age == 0 && d->owner != 0)
-	{
-		printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
-	}
-	else if (d->name != 0 && d->age != 0 && d->owner == 0)
-	{
-		printf("Name: %s\nAge: %.6f\nOwner: (nil)\n", d->name, d->age);
+		if (d->name != 0 && d->age != 0 && d->owner != 0)
+		{
+			printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
+		}
+		else if (d->name == 0 && d->age != 0 && d->owner != 0)
+		{
+			printf("Name: (nil)\nAge: %.6f\nOwner: %s\n", d->age, d->owner);
+		}
+		else if (d->name != 0 && d->age == 0 && d->owner != 0)
+		{
+			printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
+		}
+		else if (d->name != 0 && d->age != 0 && d->owner == 0)
+		{
+			printf("Name: %s\nAge: %.6f\nOwner: (nil)\n", d->name, d->age);
+		}
 	}
 }
