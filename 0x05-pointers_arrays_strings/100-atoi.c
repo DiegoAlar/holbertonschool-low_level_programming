909ad58df8c42c578b73735a259ee86a38d16ac9
Diego Alarcon <1153@holbertonschool.com>
2019-10-21 00:48:06 +0000

    fixing 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,72 +1,46 @@
 #include "holberton.h"
-
-/**
-  * positionOfNumber - returns position of first number
-  * @largeString
-  * Return: coverts string number to int
-  */
-int positionOfNumber(char *largeString)
-{
-	int i = 0, k = 0;
-
-	while (largeString[i] != 0)
-	{
-		if (largeString[i] > 47 && largeString[i] < 58)
-		{
-			k = i;
-			break;
-		}
-		i++;
-	}
-	return k;
-}
-
-/**
-  * findingSize - looks for a size of a number pattern in a string
-  * @largeString: string to evaluate
-  * Return: returns a size containing only numbers
-  */
-int findingSize(char *largeString)
-{
-	int i = positionOfNumber(largeString), count = 0;
-	int sw = 1;
-
-	while (sw == 1)
-	{
-		if (largeString[i] > 47 && largeString[i] < 58)
-		{
-			count++;
-		}
-		else
-		{
-			sw = 0;
-		}
-		i++;
-	}
-	return count;
-}
-
 /**
 * _atoi - converts a number inside a string to int
 * @s: string input
 * Return: returns the number as integer
 */
 int _atoi(char *s)
-{
-	int sizeOfNumbers, initialPosition, res = 0, mult = 1, setLimit;
+{//b = i; c = k; f = q; unsigned int z = e
+	int i, k, sign, f;
+	unsigned int z;
 
-	initialPosition = positionOfNumber(s);
-	sizeOfNumbers = findingSize(s);
-	setLimit = initialPosition + sizeOfNumbers;
+	i = 0;
+	sign = 1;
+	z = 0;
+	q = 0;
 
-	while (setLimit >= initialPosition)
+	while (s[i])
 	{
-		int valor;
-
-		valor = s[setLimit] - 48;
-		res = (valor * mult) + res;
-		mult = mult * 10;
-		setLimit--;
+		k = s[i];
+		if (k == 45)
+		{
+			sign = sign * (-1);
+		}
+		if (k >= 48 && k <= 57)
+		{
+			k = s[i] - 48;
+			if (z = 0)
+			{
+				z = k;
+			}
+			else
+			{
+				z = (z * 10) + k;
+				++q;
+			}
+		}
+		else if (q > 0)
+		{
+			break;
+		}
+		++i;
 	}
-	return res;
+	z = z * sign;
+	return (z);
 }
+
