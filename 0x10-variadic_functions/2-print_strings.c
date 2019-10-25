4cb2de3026276b82f78ae17192e5b453220189b7
Diego Alarcon <1153@holbertonschool.com>
2019-10-25 18:42:37 +0000

    adding exercise number 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+/**
+  * print_strings - print string arguments with separator in between
+  * @separator: string separator between arguments
+  * @n: number of arguments
+  *
+  *
+  */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+	char *str;
+
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(valist, char *);
+		if (str == NULL)
+			printf("nil");
+		else
+			printf("%s", str);
+		if (separator != NULL && i != (n - 1))
+			printf("%s", separator);
+	}
+	va_end(valist);
+	printf("\n");
+}
