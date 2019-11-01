dd4f498d961fe11afa3128da8a51d65e5a82598d
Diego Alarcon <1153@holbertonschool.com>
2019-11-01 04:26:56 +0000

    fixing 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,13 +7,14 @@
   */
 void free_list(list_t *head)
 {
-	list_t *temp = head;
+	list_t *temp;
 
 	while (head != NULL)
 	{
 		temp = head->next;
+		free(head->str);
 		free(head);
 		head = temp;
 	}
-
+	free(head);
 }
