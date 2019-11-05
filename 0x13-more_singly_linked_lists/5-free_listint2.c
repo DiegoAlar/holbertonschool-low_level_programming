533aabc83ba19811f0ceb8744d6f1e212d0fd7ba
Diego <diegoaav@hotmail.com>
2019-11-05 16:42:31 +0000

    adding 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+  * free_listint2 - function that frees a listint_t list.
+  * @head: ptr to head ptr
+  *
+  *
+  */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	while (temp)
+	{
+		temp = temp->next;
+		free(*head);
+		*head = temp;
+	}
+
+}
