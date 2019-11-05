746f34d985ca12ddf5de9093aedee2abe063b4bf
Diego <diegoaav@hotmail.com>
2019-11-05 20:39:52 +0000

    fixing 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (newN);
 	}
 	temp = *head;
-	while (temp->next)
+	while (temp)
 	{
 		temp = temp->next;
 		countElemts++;
