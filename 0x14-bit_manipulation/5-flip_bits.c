58ace61cb1fc901cc1492428e07b34ca27838a85
Diego <diegoaav@hotmail.com>
2019-11-08 04:23:09 +0000

    adding exercise 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+  * flip_bits - sets the value of a bit to 0 at a given index
+  * @n: number 1
+  * @m: number 2
+  * Return: number of bits to flip
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int num = 0, bits = 0;
+
+	num = (m ^ n);
+
+	while (num)
+	{
+		bits += (num & 1);
+		num >>= 1;
+
+	}
+	return (bits);
+
+
+
+}
