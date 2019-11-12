cb3ba11bbb519e2193e1e75dba0dc66fa4ce9f83
Diego <diegoaav@hotmail.com>
2019-11-12 20:19:13 +0000

    fixing 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	len = strlen(text_content);
-	opn = open(filename, O_WRONLY | O_CREAT | O_TRUNC, (S_IRWXU | S_IRUSR));
+	opn = open(filename, O_WRONLY | O_CREAT | O_TRUNC, (S_IRUSR | S_IWUSR));
 	if (opn == -1)
 		return (-1);
 
