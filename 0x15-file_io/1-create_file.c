9988521d1549673c0d55d884d9c41a28fe1f18b9
Diego <diegoaav@hotmail.com>
2019-11-12 20:15:26 +0000

    fixing 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,23 +9,16 @@
 int create_file(const char *filename, char *text_content)
 {
 	int opn, len;
-	char *buff;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		text_content = "";
 	len = strlen(text_content);
-	buff = malloc(sizeof(char) * len);
-	if (buff == NULL)
-		return (-1);
-
-	/*fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, */
-	opn = open(filename, O_CREAT | O_TRUNC, 0600);
+	opn = open(filename, O_WRONLY | O_CREAT | O_TRUNC, (S_IRWXU | S_IRUSR));
 	if (opn == -1)
 		return (-1);
-	write(opn, buff, len);
-	free(buff);
+
+	if (text_content != NULL)
+		write(opn, text_content, len);
 	close(opn);
 	return (1);
 
