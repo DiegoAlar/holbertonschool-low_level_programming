a317daa1e771b34d554db87dddb4b4027adaff61
DiegoAlar <diegoaav@hotmail.com>
2019-12-15 21:00:39 +0000

    fixing exercise 8 (betty)

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,52 +8,46 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	unsigned int i = 0;
-	dlistint_t *temp;
+	dlistint_t *temp = *head;
 	dlistint_t *temp2;
 
 	if (!head || !(*head))
 		return (-1);
 	if (!index)
 	{
-/*		if (!(*head))
-			return (-1);
+		if ((*head)->next)
+		{
+			*head = (*head)->next;
+			/*free((*head)->prev);*/
+			(*head)->prev = NULL;
+		}
 		else
-		{*/
-			if ((*head)->next)
-			{
-				*head = (*head)->next;
-				free((*head)->prev);
-				(*head)->prev = NULL;
-			}
-			else
-			{
-				free(*head);
-				*head = NULL;
-			}
-			return (1);
-	/*	}*/
+		{
+			/*free(*head);*/
+			*head = NULL;
+		}
+		return (1);
 	}
-	temp = *head;
 	while (temp->next)
 	{
 		if (i == (index - 1))
-			break;
+		{
+			if (temp->next->next)
+			{
+				temp2 = temp->next->next;
+				/*free(temp2->prev);*/
+				temp->next = temp2;
+				temp2->prev = temp;
+			}
+			else
+			{
+				/*free(temp->next);*/
+				temp->next = NULL;
+			}
+			return (1);
+		}
 		temp = temp->next;
 		i++;
 	}
-	if (index != (i + 1))
-		return (-1);
-	if (temp->next->next)
-	{
-		temp2 = temp->next->next;
-		free(temp2->prev);
-		temp->next = temp2;
-		temp2->prev = temp;
-	}
-	else
-	{
-		free(temp->next);
-		temp->next = NULL;
-	}
-	return (1);
+	return (-1);
 }
