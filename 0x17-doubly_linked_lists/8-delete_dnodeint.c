79469260a096811a04ac97fdb8d740d618f83afb
DiegoAlar <diegoaav@hotmail.com>
2019-12-15 20:48:03 +0000

    fixing exercise 8 (betty)

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -15,10 +15,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 	if (!index)
 	{
-		if (!(*head))
+/*		if (!(*head))
 			return (-1);
 		else
-		{
+		{*/
 			if ((*head)->next)
 			{
 				*head = (*head)->next;
@@ -31,7 +31,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 				*head = NULL;
 			}
 			return (1);
-		}
+	/*	}*/
 	}
 	temp = *head;
 	while (temp->next)
