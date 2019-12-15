50f845fef0e316369253332c6224316e37fa952c
DiegoAlar <diegoaav@hotmail.com>
2019-12-15 15:34:20 +0000

    fixing exercise 4 (betty)

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -5,17 +5,8 @@
   */
 void free_dlistint(dlistint_t *head)
 {
-
 	if (head == NULL)
 		return;
-	/*while (temp)
-	{
-		temp = head->next;
-		free(head);
-		head = temp;
-	}
-	free(temp);*/
-
 	while (head->next)
 	{
 		head = head->next;
