91095fd41e7f31af5957cd4dae38e733398b9dfe
DiegoAlar <diegoaav@hotmail.com>
2020-01-18 14:29:12 -0500

    adding exercise 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_create - function that creates a hash table
+  * @size: the size of the array
+  * Return: a pointer to the newly created hash table
+  *
+  */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_t;
+
+	hash_t = malloc(sizeof(hash_table_t));
+	if (!hash_t)
+		return NULL;
+	hash_t->array = calloc(size, sizeof(hash_node_t *));
+	if (!hash_t->array)
+	{
+		return NULL;
+	}
+
+	return hash_t;
+
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,19 @@
+# C language - Hash Tables
+
+This folder contains exercises about Hash Tables creation, implementation of hash functions, retrieve the index of a key, adding elements to it, and more...!
+
+
+# Files:
+0-hash_table_create.c
+1-djb2.c
+2-key_index.c
+3-hash_table_set.c
+4-hash_table_get.c
+5-hash_table_print.c
+6-hash_table_delete.c
+
+
+## Author:
+
+This directory is written and mantain by **Diego A. Alarcón V.** (1153@holbertonschool.com)
+
