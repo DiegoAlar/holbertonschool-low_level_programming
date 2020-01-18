70b35b7ed9628442d6b1a88290cf84a14f8a477c
DiegoAlar <diegoaav@hotmail.com>
2020-01-18 14:44:32 -0500

    fixing exercise 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,6 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_t = malloc(sizeof(hash_table_t));
 	if (!hash_t)
 		return (NULL);
+	hash_t->size = size;
 	hash_t->array = calloc(sizeof(hash_node_t *), size);
 	if (!hash_t->array)
 	{
