7c151b44fd83e798fd1c9e217341b6cba4c051ee
DiegoAlar <diegoaav@hotmail.com>
2020-01-19 09:33:41 -0500

    adding exercise 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,54 @@
+#include "hash_tables.h"
+
+/**
+  * free_list - frees a linked list
+  * @head: linked list to be freed
+  *
+  */
+void free_list(hash_node_t *head)
+{
+	hash_node_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->key);
+		free(head->value);
+		free(head);
+		head = temp;
+	}
+	free(head);
+}
+
+/**
+  * hash_table_delete - deletes a hash table
+  * @ht: the hash table to be deleted
+  */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int cont = 0;
+	hash_node_t *temp;
+
+	if (!ht)
+		return;
+	for (; cont < ht->size; cont++)
+	{
+		if (!ht->array[cont])
+			continue;
+		temp = ht->array[cont];
+		free_list(temp);
+	}
+	free(ht->array);
+	free(ht);
+}
+
+
+
+
+
+
+
+
+
+
