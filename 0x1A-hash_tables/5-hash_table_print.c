bb6455b511039a3bcfcd212af1994d60247b2a3b
DiegoAlar <diegoaav@hotmail.com>
2020-01-18 21:28:19 -0500

    adding exercise 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+/**
+  * hash_table_print - function that prints a hash table
+  * @ht: the hash table
+  */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int cont = 0;
+	hash_node_t *temp;
+	int flag = 0;
+
+	if (!ht)
+	{
+		printf("{}\n");
+		return;
+	}
+	printf("{");
+	for (; cont < ht->size; cont++)
+	{
+		if (!ht->array[cont])
+			continue;
+		if (flag != 0)
+			printf(", ");
+		temp = ht->array[cont];
+		flag = 1;
+		while (temp->next)
+		{
+			printf("\'%s\': \'%s\'", temp->key, temp->value);
+			printf(", ");
+			temp = temp->next;
+		}
+		printf("\'%s\': \'%s\'", temp->key, temp->value);
+	}
+	printf("}\n");
+}
